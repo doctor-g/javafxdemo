281beb582ffa8ab863c1fda6fa1fa44c9b97a0f7
Paul Gestwicki <pvgestwicki@bsu.edu>
2019-02-15 07:25:26 -0500

    Add sample unit test
    The test doesn't actually test anything; it's only there to show how tests can be executed in a Gradle project.

---
+++ src/test/java/edu/bsu/cs445/SampleTest.java
@@ -0,0 +1,12 @@
+package edu.bsu.cs445;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testConfig() {
+        Assert.assertTrue(true);
+    }
+}
